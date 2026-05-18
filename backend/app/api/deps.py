1d34aae4b1be3d9dd639c91b294a7955ce697fc8
Jakub Wytykowski <jakub.wytykowski@gmail.com>
2026-05-18 10:51:12 +0200

    add routes

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,4 +1,13 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
+
+from app.core.security import decode_access_token
+from app.models.user import User
 from app.db.session import SessionLocal
+from sqlalchemy.orm import Session
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
 def get_db():
@@ -7,3 +16,21 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    payload = decode_access_token(token)
+
+    if not payload:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    email = payload.get("sub")
+
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return user
--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 
 SECRET_KEY = "supersecret"  # później do .env
@@ -25,3 +25,11 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
 
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
