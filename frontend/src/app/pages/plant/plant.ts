151827f1ab4c10dbc0566cf81585219c0cebae4c
Jakub Wytykowski <jakub.wytykowski@gmail.com>
2026-06-15 15:09:21 +0200

    prepare plants page

--- frontend/src/app/pages/plant/plant.ts
+++ frontend/src/app/pages/plant/plant.ts
@@ -2,6 +2,11 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { PlantService } from '../../services/plant.service';
+import { PlantStore } from '../../store/plant.store';
+import { GardenStore } from '../../store/garden.store';
+import { LeftNavComponent } from '../leftNav/leftNav';
+
 interface Plant {
   id: number;
   name: string;
@@ -12,50 +17,51 @@ interface Plant {
 @Component({
   selector: 'app-plants',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, LeftNavComponent],
   templateUrl: './plant.html',
 })
 export class PlantComponent {
-  tools = signal<Plant[]>([
-    {
-      id: 1,
-      name: 'Shovel',
-      quantity: 1,
-      description: 'Used for digging',
-    },
-    {
-      id: 2,
-      name: 'Watering Can',
-      quantity: 2,
-      description: 'Used for watering plants',
-    },
-  ]);
+  searchTerm = '';
 
-  newTool = signal({
-    name: '',
-    quantity: 1,
-    description: '',
-  });
+  constructor(
+    public plantStore: PlantStore,
+    private plantService: PlantService,
+    public gardenStore: GardenStore,
+  ) {}
 
-  addTool() {
-    const tool = this.newTool();
+  ngOnInit(): void {
+    this.plantService.getAllPlants().subscribe({
+      next: (data) => {
+        this.plantStore.plants.set(data);
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
 
-    if (!tool.name.trim()) {
-      return;
+  filterPlants() {
+    const term = this.searchTerm.trim().toLowerCase();
+
+    if (!term) {
+      return this.plantStore.plants();
     }
 
-    this.tools.update((tools) => [
-      ...tools,
-      {
-        id: Date.now(),
-        ...tool,
-      },
-    ]);
+    return this.plantStore
+      .plants()
+      .filter(
+        (plant) =>
+          plant.name.toLowerCase().includes(term) || plant.species.toLowerCase().includes(term),
+      );
+  }
 
-    this.newTool.set({
-      name: '',
-      quantity: 1,
-      description: '',
-    });
+  isValidGardenName(name: string): boolean {
+    return this.gardenStore
+      .gardens()
+      .some((garden) => garden.name.toLowerCase() === name.trim().toLowerCase());
+  }
+
+  addPlantToGarden(plantName: string, gardenName: string) {
+    console.log(`Adding plant ${plantName} to garden ${gardenName}`);
   }
 }
